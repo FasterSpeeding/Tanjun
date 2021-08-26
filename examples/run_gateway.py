8e8b808894b96c3e7e92d88eadc5bcf9438c4841
Faster Speeding <luke@lmbyrne.dev>
2021-08-26 04:45:17 +0100

    Add run REST bot example

--- examples/run_gateway.py
+++ examples/run_gateway.py
@@ -22,11 +22,20 @@ async def get_prefix(
 
 def run() -> None:
     loaded_config = config.ExampleConfig.load()
+    # Note that by default `from_gateway_bot` sets `event_managed` to `True`.
+    # This means that the client will be implicitly started and stopped
+    # based on Hikari's lifetime events.
+    #
+    # You can alternatively start and stop the Tanjun client yourself
+    # by calling `open` and `close` on it or using it as a context manager.
+    #
+    # Note that starting a Tanjun client before the relevant bot instance
+    # may lead to erroneous behaviour as it won't be able to make requests.
     bot = hikari.GatewayBot(loaded_config.bot_token)
     (
         tanjun.Client.from_gateway_bot(bot)
         .load_modules("examples.complex_component")
-        .load_modules("examples.basic_component")
+        .load_modules("examples.message_component")
         .load_modules("examples.slash_component")
         .add_prefix(loaded_config.prefix)
         .set_prefix_getter(get_prefix)
@@ -34,3 +43,7 @@ def run() -> None:
         .add_type_dependency(protos.DatabaseProto, tanjun.cache_callback(impls.DatabaseImpl.connect))
     )
     bot.run()
+
+
+if __name__ == "__main__":
+    run()
---
+++ examples/run_slash.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# cython: language_level=3
+"""Example of how to run a standard Tanjun client instance."""
+import asyncio
+
+import hikari
+
+import tanjun
+from examples import config
+from examples import impls
+from examples import protos
+
+
+async def run() -> None:
+    loaded_config = config.ExampleConfig.load()
+    # While a BOT token is assumed in this example, a client credentials OAuth2
+    # token can also be used with Tanjun but this may limit functionality.
+    bot = hikari.RESTBot(loaded_config.bot_token, hikari.TokenType.BOT)
+    client = (
+        tanjun.Client.from_rest_bot(bot)
+        .load_modules("examples.slash_component")
+        .add_type_dependency(config.ExampleConfig, lambda: loaded_config)
+        .add_type_dependency(protos.DatabaseProto, tanjun.cache_callback(impls.DatabaseImpl.connect))
+    )
+    # Unlike with a gateway bot, for RESTBots hikari has no lifetime event
+    # dispatch which can be used to implicitly startup and close the Tanjun
+    # client. Instead, we must explicitly startup and close Tanjun.
+    await bot.start()
+
+    # Note that starting a Tanjun client before the relevant bot may lead
+    # to erroneous behaviour as it won't be able to make any requests.
+
+    # While this example uses the client as a context manager to implicitly start
+    # and close it, the `open` and `close` methods can alternatively be used to the same effect.
+    async with client:
+        await bot.join()  # This waits until the bot is closed before closing Tanjun by exiting the context manager.
+
+
+def main():
+    asyncio.run(run())
+
+
+if __name__ == "__main__":
+    main()
