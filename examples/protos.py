87b78e5504e30155c5fdac002d0f92eab84b54b8
Lucina <lucina@lmbyrne.dev>
2021-10-06 21:02:17 +0100

    Switch examples over to CC0

--- examples/protos.py
+++ examples/protos.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
 # cython: language_level=3
+# Tanjun Examples - A collection of examples for Tanjun.
+# Written in 2021 by Lucina Lucina@lmbyrne.dev
+#
+# To the extent possible under law, the author(s) have dedicated all copyright
+# and related and neighboring rights to this software to the public domain worldwide.
+# This software is distributed without any warranty.
+#
+# You should have received a copy of the CC0 Public Domain Dedication along with this software.
+# If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 """Example interfaces used within the examples."""
 import typing
 
