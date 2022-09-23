90b47b710d2cc4ace0c4a19ede7f1388aa800d3e
Lucina <luke@lmbyrne.dev>
2022-09-23 11:49:18 +0100

    Add changelog entry for Default (#445)

--- tanjun/annotations.py
+++ tanjun/annotations.py
@@ -330,7 +330,7 @@ class _DefaultMeta(abc.ABCMeta):
 
 
 class Default(_ConfigIdentifier, metaclass=_DefaultMeta):
-    """Explicitly configure the an argument's default.
+    """Explicitly configure an argument's default.
 
     Examples
     --------
