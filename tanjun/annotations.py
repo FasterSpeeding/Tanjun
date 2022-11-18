dacf9dbb135f8531b7551913f6746cfc4de70eae
Lucina <lucina@lmbyrne.dev>
2022-11-18 12:22:47 +0000

    Small doc update (#564)

--- tanjun/annotations.py
+++ tanjun/annotations.py
@@ -997,7 +997,7 @@ class _TheseChannelsMeta(abc.ABCMeta):
 
 
 class TheseChannels(_ConfigIdentifier, metaclass=_TheseChannelsMeta):
-    """Declare the type of channels a slash command partial channel argument can target."""
+    """Restrain the type of channels a channel argument can target."""
 
     __slots__ = ("_channel_types",)
 
