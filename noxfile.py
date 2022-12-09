fb2320065d89a0127556ab3932c214d12701c2aa
Faster Speeding <luke@lmbyrne.dev>
2022-12-09 21:40:05 +0000

    Update CI

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,6 @@
 import pathlib
 import sys
 
-sys.path.insert(1, str(pathlib.Path("./piped").absolute()))
+sys.path.insert(1, str(pathlib.Path("./piped/python").absolute()))
 
 from noxfile import *
