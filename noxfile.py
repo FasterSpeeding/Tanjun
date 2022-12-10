1f6806b3f9b97804be259f747bb463ecf5b0efd0
Faster Speeding <luke@lmbyrne.dev>
2022-12-10 22:49:21 +0000

    Fix noxfile on linux

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,6 @@
 import pathlib
 import sys
 
-sys.path.insert(1, str(pathlib.Path("./piped/python").absolute()))
+sys.path.insert(0, str(pathlib.Path(__file__).parent / "piped" / "python"))
 
 from noxfile import *
